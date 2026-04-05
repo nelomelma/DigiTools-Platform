1f72513a83bfa3326ca0287ae7f11fbd89a75fec
nelomelma <nelomelma74@gmail.com>
2026-04-06 05:07:18 +0600

    updated Code

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/DigiTools-Platform">
       <App />
     </BrowserRouter>
   </React.StrictMode>
